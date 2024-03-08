c1546566d0f1486f00f1260fa67859d60505bc8e
poilk76 <146364687+poilk76@users.noreply.github.com>
2024-03-08 20:50:02 +0100

    Bug fix

--- Sorter/sorter.py
+++ Sorter/sorter.py
@@ -4,18 +4,20 @@ import glob, os
 class Sorter:
 
     def sort(self,path_destination,extensions):
-        if os.path.exists(self.path_downloads):
+        if self.path_downloads != "" and os.path.exists(self.path_downloads):
             if not os.path.exists(path_destination):
                 os.mkdir(path_destination)
             for extension in extensions:
                 for file_path in glob.glob(self.path_downloads+"/*"+extension):
                     os.system(f'move {file_path} {path_destination}')
             print("done!")
+        else:
+            print("something goes wrong!")
 
 
     def __init__(self):
         configurator = config_module.Configurator()
-        self.config = configurator.load_config("../Sorter/config.json")
+        self.config = configurator.load_config("Sorter/config.json")
         self.path_downloads = self.config["path_downloads"]
         old_path = os.getcwd()
         os.chdir(self.path_downloads)
