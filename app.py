a4cf547396ab8061cff2188c291c6ecaca6bc2e0
poilk76 <146364687+poilk76@users.noreply.github.com>
2024-03-08 20:48:59 +0100

    Clearing up

--- app.py
+++ app.py
@@ -1,15 +1,5 @@
-from os.path import expanduser
-from pathlib import Path
-import folder_paths
 from GUI import view_main
-import glob
-home_path = ""
-downloads_path = ""
 
 def run():
     print("Welcome to TidyCobra!")
-    home_path = str(Path().home())
-    downloads_path = home_path + "/Downloads"
-    print("Downloads path is:",__name__)
-    print(glob.glob("./*.txt"))
     view_main.render_GUI()
