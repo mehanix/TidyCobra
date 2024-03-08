cff1ce9f1afd6907d4534006870bd7cfa0905dbb
poilk76 <146364687+poilk76@users.noreply.github.com>
2024-03-08 20:46:59 +0100

    Deleting unnecessary files

--- folder_paths.py
+++
@@ -1,5 +0,0 @@
-'''
-    Wrapper class that stores associations between paths and the file extensions to be moved to their respective paths.
-'''
-
-rules = dict() # rule format: "absolute_path_to_folder" : ["extensions","array"]
